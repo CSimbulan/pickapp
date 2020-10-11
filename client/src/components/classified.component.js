5aabd8592163fdb4d1636fcfb2e4b5d69f431241
csimbulan <c.simbulan@hotmail.com>
2020-10-11 11:38:19 -0400

    fixed delete redirect

--- client/src/components/classified.component.js
+++ client/src/components/classified.component.js
@@ -70,7 +70,7 @@ export default class Classified extends Component {
     >
       <i className="far fa-edit" data-toggle="tooltip" data-placement="top" title="Edit"></i>
     </Link>{" | "}<a
-      href="/profile/#"
+      href={this.getHref()}
       onClick={() => {
         this.props.deleteClassified(this.props.classified._id);
       }}
